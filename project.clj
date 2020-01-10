18df24e8d41771dbdda8ae2d9d6ab66d1a55483c
Steve Miner <steveminer@gmail.com>
2020-01-10 10:35:09 -0500

    bump lib

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/core.rrb-vector "0.1.0"]
+                 [org.clojure/core.rrb-vector "0.1.1"]
                  [org.clojure/data.avl "0.1.0"]
                  ;; [ctries.clj "0.0.2"]
                  [org.clojure/data.int-map "0.2.4"]])
